2b2c4b3c926cb06c132626eff23e6b5ca829f6e0
unknown <nadilasal1974@gmail.com>
2024-07-03 20:29:51 +0700

    update commit

--- js/script.js
+++ js/script.js
@@ -6,36 +6,66 @@ function replaceName(){
 }
 
 let tombol = document.getElementById("tombol").addEventListener("click", function(){
-    replaceName()
+    replaceName();
 }); //setiap di klik, maka panggil replace name
 
-document.getElementById("name-input").addEventListener("onKeyup", function(nama){
-    console.log(nama)
-})
+// document.getElementById("name-input").addEventListener("onKeyup", function(nama){
+//     console.log(nama)
+// })
 
-function validateForm(){
-    const nama=document.forms['message-form']['full-name'].value;
-    const birthDate = document.forms['message-form']['birth-date'].value;
-    const gender = document.forms['message-form']['gender'].value;
-    const messages =document.forms['message-form']['messages'].value;
+var slideIndex=1;
+var n;
+showDivs(slideIndex);
 
-    if(nama=='' || birthDate=='' || gender=='' || messages==''){
-        document.getElementById('error-name').innerHTML="Tidak boleh kosong"
-        allert("Tidak boleh ada yang kosong")
-        return false
+function plusDivs(n){
+    showDivs((slideIndex += n));
+}
+
+function showDivs(n){
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+    if(n>imgList.length) slideIndex=1;
+    else if(n<1) slideIndex=imgList.length;
+
+    for(i=0;i<imgList.length;i++){
+        imgList[i].style.display="none";
     }
 
-    document.getElementById("name").innerHTML=nama;
-    document.getElementById("error-name").innerHTML="";//biar error nya ilang
+    imgList[slideIndex-1].style.display="block";
+}
+
+setInterval(() => {
+    plusDivs(1);
+}, 1000) //biar jalan setiap 1000 ms atau 1s
+
+function validateForm(){
+    const nama=document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages =document.forms["message-form"]["message"].value;
+
+    if(nama=='' || birthDate=='' || gender=='' || messages==''){
+        document.getElementById('error-name').innerHTML="Tidak boleh kosong";
+        allert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    // document.getElementById("name").innerHTML=nama;
+    // document.getElementById("error-name").innerHTML="";//biar error nya ilang
 
     setSenderUI(nama, birthDate, gender, messages);
 
     return false;
 }
 
-function setName(nama, birthDate, gender, messages){
+function setSenderUI(nama, birthDate, gender, messages){
     document.getElementById("sender-full-name").innerHTML=nama;
     document.getElementById("sender-birth-date").innerHTML=birthDate;
     document.getElementById("sender-gender").innerHTML=gender;
     document.getElementById("sender-messages").innerHTML=messages;
 }
+
+//show divs buat geser geser otomatis, -1 akan mundur, +1 akan maju
+//ada looping jika i kurang dari banyak gambar, diplay di none, gk muncul gambarnya
+//display block akan dimunculkan
+
